de9726d3c4abc7ccd3e076bf393db38c87b6d14a
Blossom <saphashb@gmail.com>
2022-03-05 18:43:11 +0100

    install body parser middleware

--- build/index.js
+++ build/index.js
@@ -4,6 +4,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = (0, express_1.default)();
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
 var PORT = 3000;
+var foodTimetable = {
+    breakfast: 'tea',
+    lunch: 'sandwich',
+    dinner: 'vegetable',
+};
+app.get('/', function (req, res) {
+    res.status(200).json(foodTimetable);
+});
+app.post('/', function (req, res) {
+    console.log(req.body);
+    var data = Object.keys(req.query)[0];
+    var meal;
+    if (data === 'breakfast') {
+        meal = req.query.breakfast;
+        foodTimetable.breakfast = meal;
+    }
+    else if (data === 'lunch') {
+        meal = req.query.lunch;
+        foodTimetable.lunch = meal;
+    }
+    if (data === 'dinner') {
+        meal = req.query.dinner;
+        foodTimetable.dinner = meal;
+    }
+    try {
+        res.status(200).json(foodTimetable);
+    }
+    catch (error) {
+        console.log(error);
+        res.send('You have entered an invalid format');
+    }
+});
+app.put('/:id', function (req, res) {
+    res.send('update data on this endpoint');
+});
+app.delete('/:id', function (req, res) {
+    res.send('delete data on this endpoint');
+});
 app.listen(PORT, function () { return console.log("app is listening on port ".concat(PORT)); });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,55 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const PORT = 3000;
+
+interface Food {
+  breakfast: string;
+  lunch: string;
+  dinner: string;
+}
+let foodTimetable: Food = {
+  breakfast: 'tea',
+  lunch: 'sandwich',
+  dinner: 'vegetable',
+};
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json(foodTimetable);
+});
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  let data = Object.keys(req.query)[0];
+  let meal;
+  if (data === 'breakfast') {
+    meal = req.query.breakfast as string;
+    foodTimetable.breakfast = meal;
+  } else if (data === 'lunch') {
+    meal = req.query.lunch as string;
+    foodTimetable.lunch = meal;
+  }
+  if (data === 'dinner') {
+    meal = req.query.dinner as string;
+    foodTimetable.dinner = meal;
+  }
+
+  try {
+    res.status(200).json(foodTimetable);
+  } catch (error) {
+    console.log(error);
+    res.send('You have entered an invalid format');
+  }
+});
+
+app.put('/:id', (req: Request, res: Response) => {
+  res.send('update data on this endpoint');
+});
+
+app.delete('/:id', (req: Request, res: Response) => {
+  res.send('delete data on this endpoint');
+});
 
 app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
