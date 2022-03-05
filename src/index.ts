a2b92c5c0edac2f833547f69a70a50de15692874
Blossom <saphashb@gmail.com>
2022-03-05 17:28:32 +0100

    cleanup project and setup project structure

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-import addFunc from './controller/add';
-console.log(addFunc(6, 7));
-
-// a text editor function
-export const textEditor = (word: string): string => {
-  const newWord = word.split(' ');
-  let i = 0;
-  while (i < newWord.length) {
-    if (newWord[i] === 'movie') {
-      newWord[i] = 'film';
-    }
-    i++;
-  }
-
-  return newWord.join(' ');
-};
-
-textEditor('The movie that just came out is a phenomenal movie');
+console.log('');
