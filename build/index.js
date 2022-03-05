de9edcc01bf9e0b05208a55c1333080b36794fcd
Blossom <saphashb@gmail.com>
2022-03-05 17:30:11 +0100

    instantiate express application object

--- build/index.js
+++ build/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log('');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var PORT = 3000;
+app.listen(PORT, function () { return console.log("app is listening on port ".concat(PORT)); });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('');
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`app is listening on port ${PORT}`));
